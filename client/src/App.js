436c78d907dabd6ace9974885128321e94dfd98c
Lior Kaufman <liorka1990@gmail.com>
2020-03-05 12:49:08 -0800

    connected API GET and POST

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import { Container } from "reactstrap";
+
+//Components
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/modals/ItemModal";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div>
-      <AppNavbar />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
